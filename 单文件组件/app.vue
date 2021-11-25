7bea7a842cabc55386cc0619aa4d909f222d6050
xk <839140758@qq.com>
2021-11-25 11:25:38 +0800

    组件

---
+++ 单文件组件/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <school></school>
+  </div>
+</template>
+
+<script>
+import school from 'school'
+export default {
+    components:{
+        school
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 单文件组件/min.js
@@ -0,0 +1,8 @@
+import app from 'app'
+new Vue({
+    template:`<div><app></app></div>`,
+    el:'#root',
+    components:{
+        app
+    }
+})
